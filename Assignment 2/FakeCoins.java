e9646c1f8c93c998f38cb842f61da4944cc57ab5
Starwolf16 <jmtarrant00@gmail.com>
2022-09-27 23:10:47 -0400

    Finish FakeCoins.java

--- Assignment 2/FakeCoins.java
+++ Assignment 2/FakeCoins.java
@@ -13,46 +13,63 @@
  *      who's coin weight == 11 
  * 
  *  Algorithm Pseudocode Syntax
+ *  // Searchs for a stack of fake coins
+ *  // Input: ArrayList of weights for coin stacks
+ *  // Output: Position of stack of fake coins
+ *      for index ← 0 to stacks.length
+ *          numWeighs ← numWeighs + 1
+ *          if stacks[i] = 11
+ *              arr[0] ← index
+ *              arr[1] ← numWeighs
+ * 
+ *  Minimum Number of Weighings:
+ *      1 weighing. This is the best case scenario where the stack 
+ *      of fake coins is the first stack in the list
  *      
  */
 
 //Code Section
 
-//Imports
+//Import for Scanner and ArrayList
 import java.util.*;
 
 public class FakeCoins {
     public static void main(String []args){
+        //Create Scanner object for inputs
         Scanner input = new Scanner(System.in);
+
+        //Varable declarations
         boolean run = true;
         int userInput = 0;
         int stackCount = 0;
-        List<Integer> stacks = new ArrayList<Integer>();
+        ArrayList<Integer> stacks = new ArrayList<Integer>(0); //ArrayList to hold the weights of the stacks
+        int[] funcReturn; //Array for holding the results of the algoritm
         while(run){
             Menu();
-            userInput = input(input);
-            System.out.println(userInput);
-            // break;
+            userInput = input(input); //Take input for Menu
             switch(userInput){
-                case 1:
+                case 1: //First menu Item
                     System.out.print("Please Input the number of stacks: ");
-                    stackCount = input(input);
-                    // for(int i = 0; i < stackCount; i++){
-                    //     stacks.add();
-                    // }
+                    stackCount = input(input); //Get number of stacks 
                     break;
-                case 2:
-                    System.out.print("Please input the weight of each Stack: ");
-                    getWeights(stacks, stackCount, input);
-                    System.out.println(stacks);
+                case 2: //Second Menu Item
+                    System.out.println("\nPlease input the weight of each Stack: \n");
+                    getWeights(stacks, stackCount, input);  //Get the weight of all the stacks
                     break;
-                case 3:
-                    System.out.println("Finding Fake Stack...");
-                    findFake(stacks);
+                case 3: //Third Menu Item
+                    System.out.println("\nFinding Fake Stack...\n");
+                    funcReturn = findFake(stacks); //Algorithm Function Call
+
+                    System.out.println("Total Number of Stacks is:    " + stackCount + "\n"
+                                        + "Fake Coins Stack is stack #:  " + funcReturn[0] + "\n"
+                                        + "# of weighings required:      " + funcReturn[1]);
                     break;
-                case 4:
+                case 4: //Fouth Menue Item
                     System.out.println("Exiting Program...");
                     run = false;
+                    break;
+                default: //Default in case user enters a non-menu number
+                    System.out.println("Please Input a value between 1 and 4");
                     break;
             }
         }
@@ -62,16 +79,16 @@ public class FakeCoins {
     
     public static int input(Scanner input){
         // int userInput = 0;
-        while(!input.hasNextInt()) {
+        while(!input.hasNextInt()) { //Checks if the entered input is an integer
             input.next();
             System.out.println("Please enter an Integer");
-            System.out.print("Enter Option Number: ");
+            System.out.print("Enter Option Number: "); 
         }
-        int userInput = input.nextInt();
+        int userInput = input.nextInt(); //Get int input
         return userInput;
     }
 
-    public static void Menu() {
+    public static void Menu() { //Print out Main Menu
         System.out.print("\n-----------------MAIN MENU-------------- \n"
                             + "1. Read number of stacks/coins per stack \n"
                             + "2. Read coin weight in each stack (10 or 11 grams) \n"
@@ -80,31 +97,40 @@ public class FakeCoins {
                             + "\nEnter Option Number: ");
     }
 
-    public static List<Integer> getWeights(List<Integer> stacks, int stackCount, Scanner input){
-        int weight = 0;
-        // boolean flag = false;
+    public static ArrayList<Integer> getWeights(ArrayList<Integer> stacks, int stackCount, Scanner input){
+        int weight = 0; //Variable to store weight while checking the value
         for (int i = 0; i < stackCount; i++){
-            System.out.print("What is the weight of the coins in stack " + (i + 1) + " in grams? ");
-            weight = Integer.valueOf(input(input));
             while (true){
-                if (weight == 11 && !stacks.contains(11)){
-                    stacks.add(weight);
-                    // flag = true;
+                System.out.print("What is the weight of the coins in stack " + (i + 1) + " in grams? ");
+                weight = input(input); //Take input weight
+                if (weight == 11 && !stacks.contains(11)){ //If input is 11, check if it's already in the ArrayList
+                    stacks.add(weight); //Add to ArrayList
                     break;
-                } else if(weight == 10) {
-                    stacks.add(weight);
-                    // flag = true;
+                } else if(weight == 10) { //Add to list if value is 10
+                    stacks.add(weight); 
                     break;
-                } else {
+                } else if (weight == 11 && stacks.contains(11)){ //If value is 11 and 11 is already in list, tell user only one can be in list
                     System.out.println("Only one stack can weigh 11 grams");
+                } else { //If value is not 10 or 11, Tell user weights can only be 10 or 11
+                    System.out.println("Please input either 10 or 11 grams");
                 }
             }
-            // stacks.add(weight);
         }
         return stacks;
     }
 
-    public static int findFake(List<Integer> stacks) {
-        return 0;
+    public static int[] findFake(ArrayList<Integer> stacks) {
+        int numWeighs = 0; //Store number of iterations through loop
+        int[] funcReturn = {0, 0}; //Array to pass both fake coin index and number of weighs
+        for (int index = 0; index < stacks.size(); index++){
+            numWeighs++;
+            if (stacks.get(index) == 11){ //Check to see if the current stack being looked at has a weight of 11
+                funcReturn[0] = index + 1;
+                funcReturn[1] = numWeighs;
+                return funcReturn; //Return results back to main()
+            }
+        } 
+        return funcReturn; //Return {0, 0} if there are no stacks with 11 coins
+
     }
 }
