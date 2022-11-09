6f4be407bb2af633d098af95dd04ab7980417c31
Starwolf16 <jmtarrant00@gmail.com>
2022-11-08 23:14:58 -0500

    Update Quicksort.java

--- Assignment 5/Quicksort.java
+++ Assignment 5/Quicksort.java
@@ -1,3 +1,49 @@
+
 public class Quicksort {
+    private int low = 0;
+    private int high = 1;
+    private int[] array;
+    private int compCount = 0;
     
+    public Quicksort(int[] array){
+        this.high = array.length - 1;
+        this.array = array.clone();
+
+        QS(this.array, this.low, this.high);
+    }
+
+    private int partition(int[] array, int low, int high){
+        int temp = 0;
+        int pivot = array[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++){
+            if (array[j] <= pivot){
+                compCount++;
+                i++;
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        temp = array[i+1];
+        array[i+1] = high;
+        array[high] = temp;
+
+        return (i + 1);
+    }
+
+    private void QS(int[] array, int low, int high){
+        if (low < high){
+            int pivot = partition(array, low, high);
+
+            QS(array, low, pivot - 1);
+            QS(array, pivot + 1, high);
+        }
+    } 
+
+    // getter for quicksort comparison count
+    public int getCompCount(){
+        return compCount;
+    }
 }
