fd93e1f9976b38319a2e73b58f60dfd07829ca25
Starwolf16 <jmtarrant00@gmail.com>
2022-11-10 14:37:39 -0500

    Update testAdvancedSorting.java

--- Assignment 5/testAdvancedSorting.java
+++ Assignment 5/testAdvancedSorting.java
@@ -25,6 +25,19 @@ public class testAdvancedSorting {
             incArray1, incArray2, incArray3, incArray4, 
             decArray1, decArray2, decArray3, decArray4};
 
+        Quicksort randQS1 = new Quicksort(), randQS2 = new Quicksort(), randQS3 = new Quicksort(), randQS4 = new Quicksort();
+        Mergesort randMS1 = new Mergesort(), randMS2 = new Mergesort(), randMS3 = new Mergesort(), randMS4 = new Mergesort();
+        Heapsort randHS1 = new Heapsort(), randHS2 = new Heapsort(), randHS3 = new Heapsort(), randHS4 = new Heapsort();
+
+        Quicksort incQS1 = new Quicksort(), incQS2 = new Quicksort(), incQS3 = new Quicksort(), incQS4 = new Quicksort();
+        Mergesort incMS1 = new Mergesort(), incMS2 = new Mergesort(), incMS3 = new Mergesort(), incMS4 = new Mergesort();
+        Heapsort incHS1 = new Heapsort(), incHS2 = new Heapsort(), incHS3 = new Heapsort(), incHS4 = new Heapsort();
+
+        Quicksort decQS1 = new Quicksort(), decQS2 = new Quicksort(), decQS3 = new Quicksort(), decQS4 = new Quicksort();
+        Mergesort decMS1 = new Mergesort(), decMS2 = new Mergesort(), decMS3 = new Mergesort(), decMS4 = new Mergesort();
+        Heapsort decHS1 = new Heapsort(), decHS2 = new Heapsort(), decHS3 = new Heapsort(), decHS4 = new Heapsort();
+
+
         while (run){
             Menu();
             question = "Enter Option Number: ";
@@ -45,26 +58,53 @@ public class testAdvancedSorting {
                     }
                     break;
                 case 2:
-                    Quicksort randQS1 = new Quicksort(randArray1);
-                    Quicksort randQS2 = new Quicksort(randArray2);
-                    Quicksort randQS3 = new Quicksort(randArray3);
-                    Quicksort randQS4 = new Quicksort(randArray4);
+                    randQS1.setArray(randArray1);
+                    randQS2.setArray(randArray2);
+                    randQS3.setArray(randArray3);
+                    randQS4.setArray(randArray4);
+                    incQS1.setArray(incArray1);
+                    incQS2.setArray(incArray2);
+                    incQS3.setArray(incArray3);
+                    incQS4.setArray(incArray4);
+                    decQS1.setArray(decArray1);
+                    decQS2.setArray(decArray2);
+                    decQS3.setArray(decArray3);
+                    decQS4.setArray(decArray4);
 
-                    Quicksort incQS1 = new Quicksort(incArray1);
-                    Quicksort incQS2 = new Quicksort(incArray2);
-                    Quicksort incQS3 = new Quicksort(incArray3);
-                    Quicksort incQS4 = new Quicksort(incArray4);
+                    randMS1.setArray(randArray1);
+                    randMS2.setArray(randArray2);
+                    randMS3.setArray(randArray3);
+                    randMS4.setArray(randArray4);
+                    incMS1.setArray(incArray1);
+                    incMS2.setArray(incArray2);
+                    incMS3.setArray(incArray3);
+                    incMS4.setArray(incArray4);
+                    decMS1.setArray(decArray1);
+                    decMS2.setArray(decArray2);
+                    decMS3.setArray(decArray3);
+                    decMS4.setArray(decArray4);
 
-                    Quicksort decQS1 = new Quicksort(decArray1);
-                    Quicksort decQS2 = new Quicksort(decArray2);
-                    Quicksort decQS3 = new Quicksort(decArray3);
-                    Quicksort decQS4 = new Quicksort(decArray4);
+                    randHS1.setArray(randArray1);
+                    randHS2.setArray(randArray2);
+                    randHS3.setArray(randArray3);
+                    randHS4.setArray(randArray4);
+                    incHS1.setArray(incArray1);
+                    incHS2.setArray(incArray2);
+                    incHS3.setArray(incArray3);
+                    incHS4.setArray(incArray4);
+                    decHS1.setArray(decArray1);
+                    decHS2.setArray(decArray2);
+                    decHS3.setArray(decArray3);
+                    decHS4.setArray(decArray4);
 
                     break;
                 case 3:
                     System.out.println("Array Type: Random");
                     System.out.println("Algorithm     n=1000     n=10000     n=100000     n=1000000");
                     System.out.println("-----------------------------------------------------------");
+                    System.out.println("MergeSort     " + randMS1.getCompCount() + "       " + randMS2.getCompCount() + "       " + randMS3.getCompCount() + "       " + randMS4.getCompCount());
+                    System.out.println("Quicksort     " + randQS1.getCompCount() + "       " + randQS2.getCompCount() + "       " + randQS3.getCompCount() + "       " + randQS4.getCompCount());
+                    
                                      
                     break;
                 case 4:
