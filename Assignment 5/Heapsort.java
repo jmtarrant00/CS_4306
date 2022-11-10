357b8025aeff47e3ff232fd5396ebfc014b7a7fa
Starwolf16 <jmtarrant00@gmail.com>
2022-11-10 16:14:38 -0500

    Update Heapsort.java

--- Assignment 5/Heapsort.java
+++ Assignment 5/Heapsort.java
@@ -1,5 +1,4 @@
 
-
 public class Heapsort {
     private int[] array;
     private int compCount = 0;
@@ -10,5 +9,55 @@ public class Heapsort {
 
     public void setArray(int[] array){
         this.array = array;
+
+        HS(this.array);
+    }
+
+    private void HS(int[] array){
+        int n = array.length;
+        int temp;
+
+        for (int i = (n/2-1); i >= 0; i--){
+            heapify(array, n, i);
+        }
+
+        for (int i = n-1; i > 0; i--){
+            temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            heapify(array, i, 0);
+        }
+    }
+
+    private void heapify(int[] array, int n, int i){
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int swap;
+
+        if (left < n && array[left] > array[largest]){
+            largest = left;
+            compCount++;
+        }
+
+        if (right < n && array[right] > array[largest]){
+            largest = right;
+            compCount++;
+        }
+
+        if (largest != i){
+            compCount++;
+            swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+
+            heapify(array, n, largest);
+        }
+
+       
+    }
+
+    public int getCompCount(){
+        return compCount;
     }
 }
