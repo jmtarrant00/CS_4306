f1627b1d1dd19b899f802617fd8a8efde2800b09
Starwolf16 <jmtarrant00@gmail.com>
2022-11-10 14:37:20 -0500

    Update Heapsort.java

--- Assignment 5/Heapsort.java
+++ Assignment 5/Heapsort.java
@@ -1,3 +1,14 @@
+
+
 public class Heapsort {
-    
+    private int[] array;
+    private int compCount = 0;
+
+    public Heapsort(){
+
+    }
+
+    public void setArray(int[] array){
+        this.array = array;
+    }
 }
