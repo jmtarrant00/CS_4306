b7f95caf6be87e2198252a8545e98c11917d8dc5
Starwolf16 <jmtarrant00@gmail.com>
2023-10-16 22:01:11 -0400

    Remove extraneous whitespace

--- Second_Class/Midterm_Files/Question_5.py
+++ Second_Class/Midterm_Files/Question_5.py
@@ -1,33 +1,25 @@
 class Array(object):
-
    def __init__(self, initialSize):    # Constructor
       self.array = [None] * initialSize  # The array stored as a list
       self.num_items = 0                # No items in array initially
 
- 
-
    def __len__(self):                  # Special def for len() func
       return self.num_items             # Return number of items
-
-   
 
    def get(self, n):                   # Return the value at index n
       if 0 <= n and n < self.num_items: # Check if n is in bounds, and
          return self.array[n]            # only return item if in bounds
 
-   
 
    def set(self, n, value):            # Set the value at index n
       if 0 <= n and n < self.num_items: # Check if n is in bounds, and
          self.array[n] = value           # only set item if in bounds
 
-      
 
    def insert(self, item):             # Insert item at end
       self.array[self.num_items] = item   # Item goes at current end
       self.num_items += 1               # Increment number of items
 
- 
 
    def find(self, item):               # Find index for item
       for j in range(self.num_items):   # Among current items
@@ -36,12 +28,9 @@ class Array(object):
 
       return -1                        # Not found -> return -1
 
-   
-
    def search(self, item):             # Search for item
       return self.get(self.find(item)) # and return item if found
 
- 
 
    def delete(self, item):             # Delete first occurrence
       for j in range(self.num_items):   # of an item
@@ -52,8 +41,6 @@ class Array(object):
             return True                # Return success flag
 
       return False     # Made it here, so couldn't find the item   
-
- 
 
    def traverse(self, function=print): # Traverse all items
       for j in range(self.num_items):   # and apply a function
